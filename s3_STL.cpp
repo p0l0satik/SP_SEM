fe115975822818ca2126b0986c61c4a1e10e1520
polosatik <denis.cucu@yandex.ru>
2019-03-04 10:34:27 +0300

    upd

--- s3_STL.cpp
+++ s3_STL.cpp
@@ -11,34 +11,44 @@ using namespace std;
 
 // array -  фиксированный на этапе компиляции массив
 // string
-// элементы с одинаковыми значением[multi]set - двоичное дерево поиска(обычно - красночерное); множество 
+// элементы с одинаковыми значением[multi]set - двоичное дерево поиска(обычно - красночерное); множество
 // map - ассоциативный массив, тож массив
 // unordered_set[map] хэщ таблички - нужны хэши, сравнение на эквивалентность
 
-class MyVector
-{
-	int buf[10];
-	int size;
-public:
-	MyVector(std::initializer_list<int> val) 
-	{
-		int i = 0;
-
-		for (auto elem : val)
-		{
-			buf[i] = elem;
-			++i;
-		}
-		size = val.size();
+class MyVector {
+	std::string *buf;
+	int size, capacity;
+  public:
+	MyVector() {
+		size = 0;
+		capacity = 0;
 	}
 
-	int *begin()
-	{
+	void push_back(const std::string& s) {
+		if (size >= capacity) {
+			//bad code
+			capacity = 2 * capacity + 1;
+			// std::string *buf = new std::string[capacity]; //создать capacity новых объектов - ЗЛО!
+			std:: string *tmp = malloc(sizeof(std::string) * capacity);
+			for (int i = 0; t < size; ++i) {
+				new(tmp + i) std::string(buf[1]);// над куском выделяем конструктор
+				// tmp[i] = buf[i]; // c malloc так нельзя
+			}
+			//основная идея - не вызывать контсруктор для пустой памяти!!!
+			for (int i = 0; i < size; ++i) {
+				buf[i].~string();
+			}
+			free(buf)
+			// delete[] buf;
+			buf = tmp;
+		}
+		new (buf + size) string(s);
+	}
+	int *begin() {
 		return buf;
 	}
 
-	int *end()
-	{
+	int *end() {
 		return buf + size;
 	}
 };
@@ -46,8 +56,7 @@ public:
 int main() {
 	std::vector<int> v = {1, 4, 5}; //v(3)
 
-	for(auto  elem: v) 
-	{
+	for (auto  elem : v) {
 		std::cout << elem;
 	}
 	std::cout << endl;
@@ -60,7 +69,7 @@ int main() {
 	/*итераторы
 	// [) полуоткрытые диапазаоны
 	// begin, end - указатели на начало ик онец контейнера
-	// 
+	//
 	it = v.begin()
 	it++;
 
@@ -71,7 +80,7 @@ int main() {
 		*
 		+=
 		<
-	
+
 		Bidirectional
 		для списка
 		++
@@ -84,9 +93,9 @@ int main() {
 
 		Forward
 		++*
-		
 
-		реверс итератор 
+
+		реверс итератор
 		rbegin
 		rend
 
@@ -96,23 +105,31 @@ int main() {
 		итератор может указывать только на элемент или на следующий с конца, иначе undefined behaviour
 	*/
 
-	for(auto  elem: v) 
-	{
+	for (auto  elem : v) {
 		std::cout << elem;
 	}
 	std::cout << endl;
-	MyVector v1{1, 4, 3};
 
-	for (auto elem: v1)
+	MyVector v1;
+
+	/*
+	struct t
 	{
+		int a, b;
+	};
+	v1.push_back(t{1,2});
+	v1.emplace_back(1, 2); // не хотим временный объект
+
+	*/
+
+	for (auto elem : v1) {
 		std::cout << elem;
 	}
 	std::cout << endl;
 
 	std::sort(v1.begin(), v1.end());
 
-	for (auto elem: v1)
-	{
+	for (auto elem : v1) {
 		std::cout << elem;
 	}
 	std::cout << endl;
@@ -120,29 +137,40 @@ int main() {
 	//reverse
 
 	std::sort(v.rbegin(), v.rend());
-	for (auto elem: v)
-	{
+	for (auto elem : v) {
 		std::cout << elem;
 	}
 	std::cout << endl;
 
 
-	for(auto it = v.begin(); it < v.end(); it += 3)
-	{
+	for (auto it = v.begin(); it < v.end(); it += 3) {
 		std::cout << *it;
 	}
 	std::cout << endl;
 //=
 //=
-	for(vector<int>::const_iterator it = v.begin(); it < v.end(); it += 3)
-	{
+	for (vector<int>::const_iterator it = v.begin(); it < v.end(); it += 3) {
 		std::cout << *it;
 	}
 	std::cout << endl;
 
-	auto it = v.beign();
-	v.push_back(5);
-	std::cout << *it;
-	
+	auto it = v.begin();
+	for (int i = 0; i < 100; ++i) {
+		v.push_back(5);
+	}
+	// std::cout << *it; - он стал говном ибо память съъехала
+
+
+	std::cout << v.size() << std::endl; //number of elemnts
+	std::cout << v.capacity() << std::endl; //size of array
+
+	v.reserve(1000); // выделяет доп память под элементы чтоб добавлять хорошо
+	v.resize(10); // изменяет размер - с удалением - но память от резерва остается
+	v.shrink_to_fit(); // возращает резер памяти
+	std::cout << v.capacity() << std::endl; //size of array
+
+
+	// стандартные алгоритмы не меняют режимы контейнеров
+	//ы
 	return 0;
 }
